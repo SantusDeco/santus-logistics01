433ac170f942918ad7a8643b760eff733d05cfd8
SantusDeco <santusdeco250@gmail.com>
2026-07-29 18:12:43 +0100

    Complete premium Service page

---
+++ service.js
@@ -0,0 +1,9 @@
+const menuToggle=document.getElementById("menuToggle");
+
+const navLinks=document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click",()=>{
+
+navLinks.classList.toggle("active");
+
+});
